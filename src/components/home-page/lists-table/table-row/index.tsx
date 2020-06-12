386f803ec3afb25319b03154597819fc7da1dab3
Sasha Kirillov <sasha.anaximander@gmail.com>
2020-06-12 21:28:14 +0300

    add accordion behavior

--- src/components/home-page/lists-table/table-row/index.tsx
+++ src/components/home-page/lists-table/table-row/index.tsx
@@ -1,6 +1,11 @@
 import React, { useState, ReactElement } from 'react';
+import { createPortal } from 'react-dom';
+import { Link } from 'react-router-dom';
+import { useMediaPredicate } from 'react-media-hook';
+import AnimateHeight from 'react-animate-height';
 import { EditableCell } from 'components/common';
-import { FiPlusCircle, FiChevronDown } from 'react-icons/fi';
+import { FiChevronDown, FiMapPin } from 'react-icons/fi';
+import { IoLogoFoursquare } from 'react-icons/io';
 import './style.css';
 
 interface TableRowProps {
@@ -20,41 +25,69 @@ const TableRow = ({
 }: TableRowProps): ReactElement => {
     const noLocation = location.length <= 1;
     const [isOpen, setOpen] = useState<boolean>(false);
+    const [detailsHeight, setHeight] = useState<number>(0);
+    const isMobile = useMediaPredicate('(max-width: 900px)');
 
     const toggleRow = () => {
+        isOpen ? setHeight(0) : setHeight(70);
         setOpen(!isOpen);
     };
 
     const rowClasses = ['table-row'];
     if (noLocation) rowClasses.push('table-row-disabled');
-    if (isOpen) rowClasses.push('table-row-open');
+    isOpen ? rowClasses.push('table-row-open') : rowClasses.push('table-row-closed');
+
+    const renderToggleIcon = () => {
+        const parentSelector = isMobile ? `row-wrapper-${listIndex}` : `desktop-icon-${listIndex}`;
+        const parent = document.getElementById(parentSelector);
+        return parent !== null
+            ? createPortal(
+                  <FiChevronDown className="table-icon toggle-icon" onClick={toggleRow} />,
+                  parent,
+              )
+            : null;
+    };
 
     return (
-        <li className={rowClasses.join(' ')}>
-            <div className="col col-1" data-label="List">
-                {list.name}
-            </div>
-            <div className="col col-2" data-label="Location">
-                {isEditingMode ? (
-                    <EditableCell
-                        value={location}
-                        placeholder="List location"
-                        row={listIndex}
-                        recordData={recordData}
-                    />
-                ) : (
-                    location
-                )}
-            </div>
-            <div className="col col-3" data-label="# of places">
-                {list.itemsCount}
-            </div>
-            <div className="col col-4" data-label="Date created">
-                {new Date(list.createdAt * 1000).toLocaleDateString('ru-RU')}
-            </div>
-            <FiPlusCircle className="table-icon desktop-icon" onClick={toggleRow} />
-            <FiChevronDown className="table-icon mobile-icon" onClick={toggleRow} />
-        </li>
+        <div id={`row-wrapper-${listIndex}`} className="table-row-wrapper">
+            <li className={rowClasses.join(' ')}>
+                <div className="col col-1" data-label="List">
+                    {list.name}
+                </div>
+                <div className="col col-2" data-label="Location">
+                    {isEditingMode ? (
+                        <EditableCell
+                            value={location}
+                            placeholder="List location"
+                            row={listIndex}
+                            recordData={recordData}
+                        />
+                    ) : (
+                        location
+                    )}
+                </div>
+                <div className="col col-3" data-label="# of places">
+                    {list.itemsCount}
+                </div>
+                <div className="col col-4" data-label="Date created">
+                    {new Date(list.createdAt * 1000).toLocaleDateString('ru-RU')}
+                </div>
+                <div id={`desktop-icon-${listIndex}`} className="col-5"></div>
+            </li>
+            <AnimateHeight duration={300} height={detailsHeight}>
+                <div className="table-row-details">
+                    <div className="col link-block link-block-left" data-label="Original list">
+                        <IoLogoFoursquare />
+                        <a href={list.url}>open in Foursquare</a>
+                    </div>
+                    <div className="col link-block link-block-right" data-label="Details">
+                        <FiMapPin />
+                        <Link to="/">show details</Link>
+                    </div>
+                </div>
+            </AnimateHeight>
+            {renderToggleIcon()}
+        </div>
     );
 };
 
